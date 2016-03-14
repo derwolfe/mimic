ed480071108866bfd370b6d7acf3ea0debccb3fa
Chris Wolfe <chriswwolfe@gmail.com>
2016-03-14 18:56:02 -0500

    load plugins from the interal-plugins package if present

--- mimic/plugins/__init__.py
+++ mimic/plugins/__init__.py
@@ -1,3 +1,7 @@
 """
 This package contains individual IAPIMock plugins for mimic.
+
+It also loads any and all plugins located in your python's path.
 """
+from twisted.plugin import pluginPackagePaths
+__path__.extend(pluginPackagePaths('mimic-internal-plugins.plugins'))
